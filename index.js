e8b649fe10bcbf5802c5b099fa189f728980f4a4
Lu Nelson <lunelson@gmail.com>
2016-05-10 10:17:51 +0200

    allow SVG files as XML

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ module.exports = function (opts) {
   opts.preserveComments = opts.hasOwnProperty('preserveComments') ? opts.preserveComments : false;
   opts.verbose = process.argv.indexOf('--verbose') !== -1;
 
-  var validExts    = ['xml', 'json', 'css', 'sql'];
+  var validExts    = ['xml', 'svg', 'json', 'css', 'sql'];
   var methodSuffix = opts.type === 'minify' ? 'min' : '';
 
   return through.obj(function (file, enc, cb) {
@@ -42,6 +42,7 @@ module.exports = function (opts) {
     }
 
     try {
+      fileExt = (fileExt === 'svg') ? 'xml' : fileExt;
       file.contents = new Buffer(pd[fileExt+methodSuffix](String(file.contents), opts.preserveComments));
     } catch (err) {
       return cb(new gutil.PluginError('gulp-pretty-data', err, opts));
